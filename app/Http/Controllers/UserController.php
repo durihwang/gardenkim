baba0cb634fbc6cc75d7e43387f28782ae5d2f3b
황두리 <hdr@gabia.com>
2021-11-17 09:47:41 +0900

    controller 추가

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,11 +7,18 @@ use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index()
     {
         return view('index');
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function store(Request $request)
     {
         Log::info($request);
