243de49668ecae143ba77ccd1849254c8d3d637f
황두리 <hwangduli0515@gmail.com>
2021-11-21 00:50:14 +0900

    ssl

--- ssl.sh
+++ ssl.sh
@@ -7,7 +7,7 @@ fi
 
 domains="woori-partners.co.kr"
 rsa_key_size=4096
-data_path="../volumes/certbot"
+data_path="./volumes/certbot"
 email="kjwseaa@naver.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
