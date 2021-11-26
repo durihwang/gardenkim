f55cf1d927060960a9a0d59df3e0e000d21d040f
황두리 <hdr@gabia.com>
2021-11-26 14:39:40 +0900

    admin 수정

--- app/Admin/Controllers/MemberController.php
+++ app/Admin/Controllers/MemberController.php
@@ -10,6 +10,7 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class MemberController extends AdminController
 {
@@ -34,14 +35,18 @@ class MemberController extends AdminController
         $grid->column('phone', trans('핸드폰번호'));
         $grid->column('insurance', trans('4대보험유무'));
         $grid->column('income', trans('연봉'))->display(function (){
-            return number_format($this->income);
+            return number_format($this->income)."원";
         });
         $grid->column('loan_amount', trans('총부채금액'))->display(function (){
-            return number_format($this->loan_amount);
+            return number_format($this->loan_amount)."원";
         });
         $grid->column('overdue', trans('최근연체유무'));
-        $grid->column('created_at', trans('등록일자'));
-        $grid->column('updated_at', trans('변경일자'));
+        $grid->column('created_at', trans('등록일자'))->display(function (){
+            return date('Y-m-d H:i:s', strtotime($this->created_at));
+        });
+        $grid->column('updated_at', trans('변경일자'))->display(function (){
+            return date('Y-m-d H:i:s', strtotime($this->updated_at));
+        });
 
         return $grid;
     }
